cb0fd98be323f4f056a0004e3e7aff508f89fafc
Ronieva <148862741+Ronieva@users.noreply.github.com>
2026-02-26 15:54:28 +0100

    Update ingest.py

--- app/rag/ingest.py
+++ app/rag/ingest.py
@@ -1,8 +1,8 @@
 import uuid
 from typing import Optional, Tuple, List, Dict, Any
 
-from langchain.schema import Document
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
 
 from app.rag.vectorstore import vectorstore
